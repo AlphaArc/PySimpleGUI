a86f8cfa3b6ef99c7142e3f22c44adcadd11f9c7
AlphaArc <53635590+AlphaArc@users.noreply.github.com>
2019-08-03 15:14:33 +0530

    Add files via upload

--- GUI_2.py
+++ GUI_2.py
@@ -3,13 +3,13 @@ list1=[]
 list2=[]
 col = [
         [sg.Listbox(enable_events=True,values=list2, size=(30, 6),key="Lst2")],
-        [sg.Button("ADD",key="Add")],
-        [sg.Button("Complete",key="Com")],
-        [sg.Button("DEL",key="Del")]
       ]
 layout = [
         [sg.Listbox(enable_events=True,values=list1, size=(30, 6),key="Lst"),sg.Column(col)],
         [sg.Text('Enter your task'),sg.InputText("", key='task')],
+        [sg.Button("ADD",key="Add"),
+        sg.Button("Complete",key="Com"),
+        sg.Button("DEL",key="Del")],
         [sg.Button("Exit")]]
 window = sg.Window('My first GUI', layout)
 button, values = window.Read()
